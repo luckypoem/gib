2f2b5f9922944b702b6f3a0789f50cefdbb8bf58
luckypoem <luckypoem@gmail.com>
2024-12-10 10:27:10 -0800

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 namespace NodeJS {
   interface ProcessEnv {
-    GITHUB_CLIENT_ID: string;
-    GITHUB_CLIENT_SECRET: string;
-    AUTHOR_NAME: string;
-    BLOG_TITLE: string;
-    BLOG_DESCRIPTION: string;
-    NEXT_PUBLIC_OWNER: string;
-    NEXT_PUBLIC_REPO: string;
+    GITHUB_CLIENT_ID: Ov23liq0VjxWPMAVb8Ig;
+    GITHUB_CLIENT_SECRET: d9ca3c22bf57be1920a5cf47c4baa7c960981d36;
+    AUTHOR_NAME: brite;
+    BLOG_TITLE: blog of brite;
+    BLOG_DESCRIPTION: it's blog of brite;
+    NEXT_PUBLIC_OWNER: briteming;
+    NEXT_PUBLIC_REPO: gib;
   }
 }
